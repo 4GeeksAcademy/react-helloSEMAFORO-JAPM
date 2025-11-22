64db7834b5097fd0a1c055c2a914ace032ae5367
Japmjr <chuchitoperdomo@gmail.com>
2025-11-22 11:46:46 +0000

    parte1

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,7 +2,22 @@ import React from "react";
 
 
 const Semaforo = () => {
-	
+    const [encendido, setEncendido] = useState("red")
+    const changeColor = (color) => {
+        setEncendido(color)
+    }
+    return (
+        <main>
+            <div className="hanger"></div>
+            <div className="box">
+                <div
+                    onClick={()=> changeColor("red")}
+                    className={`red ${encendido == "red" ? "light-on" : ""}`}>
+                </div>
+                <div
+                    onClick={() => changeColor("yellow")}
+                    className={`yellow ${encendido == "yellow" ? "light-on" : ""}`}>
+                </div>
 
 
 
