a5b401aea65938db1ed47cb7f54d5615fe4225cf
Japmjr <chuchitoperdomo@gmail.com>
2025-11-22 22:13:27 +0000

    arreglo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,34 +1,20 @@
 import React from "react";
 
-
-const Semaforo = () => {
-    const [prendido, setPrendido] = useState("red")
-    const changeColor = (color) => {
-        setPrendido(color)
-    }
-    return (
-        <main>
-            <div className="hanger"></div>
-            <div className="box">
-                <div
-                    onClick={()=> changeColor("red")}
-                    className={`red ${prendido == "red" ? "light-on" : ""}`}>
-                </div>
-                <div
-                    onClick={() => changeColor("yellow")}
-                    className={`yellow ${prendido == "yellow" ? "light-on" : ""}`}>
-                </div>
-                 <div
-                    onClick={() => changeColor("green")}
-                    className={`green ${prendido == "green" ? "light-on" : ""}`}>
-                </div>
-                </div>
-                </main>
-                );
-            };
-    export default Semaforo;
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+import Semaforo from "./Semaforo";
 
 
+//create you first component
+const Home = () => {
+    return(
+        <div className = "text-center">
+            <Semaforo/>
+        </div>
+          );
+};
+
+export default Home;
 
 
 
---
+++ src/js/components/Semaforo.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+const Semaforo = () => {
+    const [prendido, setPrendido] = useState("red")
+    const changeColor = (color) => {
+        setPrendido(color)
+    }
+    return (
+        <main>
+            <div className="hanger"></div>
+            <div className="box">
+                <div
+                    onClick={()=> changeColor("red")}
+                    className={`red ${prendido == "red" ? "light-on" : ""}`}>
+                </div>
+                <div
+                    onClick={() => changeColor("yellow")}
+                    className={`yellow ${prendido == "yellow" ? "light-on" : ""}`}>
+                </div>
+                 <div
+                    onClick={() => changeColor("green")}
+                    className={`green ${prendido == "green" ? "light-on" : ""}`}>
+                </div>
+                </div>
+                </main>
+                );
+            };
+    export default Semaforo;
